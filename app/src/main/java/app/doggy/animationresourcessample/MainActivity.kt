e7a971b4d54ba9d3455fb00ba20b242f0ff2db5c
Kaito-Dogi <kendobu0405@gmail.com>
2021-11-21 09:17:24 +0900

    add: 停止機能を追加

--- app/src/main/java/app/doggy/animationresourcessample/MainActivity.kt
+++ app/src/main/java/app/doggy/animationresourcessample/MainActivity.kt
@@ -13,12 +13,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
+        // dice_image_viewの背景にdice_animationを設定
         binding.diceImageView.setBackgroundResource(R.drawable.dice_animation)
 
+        // dice_image_viewの背景に設定したdice_animationを取得
         val diceAnimation = binding.diceImageView.background
+
+        // dice_image_viewをクリックした時の処理
         binding.diceImageView.setOnClickListener {
+            // diceAnimationがAnimatableの場合
             if (diceAnimation is Animatable) {
-                diceAnimation.start()
+                if (diceAnimation.isRunning) {
+                    // diceAnimationが再生している場合，停止する．
+                    diceAnimation.stop()
+                } else {
+                    // diceAnimationが停止している場合，再生する．
+                    diceAnimation.start()
+                }
             }
         }
     }
