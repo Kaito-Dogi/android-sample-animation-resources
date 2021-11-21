4621c93ae0edf6a6a51a4da277c2a5e9e76de213
Kaito-Dogi <kendobu0405@gmail.com>
2021-11-21 09:00:39 +0900

    add: viewBindingを有効化

--- app/src/main/java/app/doggy/animationresourcessample/MainActivity.kt
+++ app/src/main/java/app/doggy/animationresourcessample/MainActivity.kt
@@ -2,10 +2,23 @@ package app.doggy.animationresourcessample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.doggy.animationresourcessample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        binding.diceImageView.setBackgroundResource(R.drawable.dice_animation)
+
+        val diceAnimation = binding.diceImageView.background
+        binding.diceImageView.setOnClickListener {
+            if (diceAnimation is Animatable) {
+                diceAnimation.start()
+            }
+        }
     }
 }
